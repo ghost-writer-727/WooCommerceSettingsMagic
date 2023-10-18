07f6e051c59e8239b3d71f236aef7474de3070cf
ghost-writer-727 <4065416+ghost-writer-727@users.noreply.github.com>
2023-10-18 15:13:56 -0400

    Remove unneeded code

--- js/select2Support.js
+++ js/select2Support.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function($) {
     $('.wcsm-apply-select2').each(function() {
-        var placeholderText = $(this).data('placeholder');
-        var defaultValue = $(this).data('default');
-        
+        var placeholderText = $(this).data('placeholder');        
         $(this).selectWoo({
             placeholder: placeholderText,
             allowClear: true
